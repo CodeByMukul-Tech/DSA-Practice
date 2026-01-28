3e03607b963a7467a10c627674476866e7d07318
CodeByMukul-Tech <mukulvats200@gmail.com>
2026-01-28 23:40:49 +0530

    Complete it

--- HashMap/src/ProblemDay2/q.java
+++ HashMap/src/ProblemDay2/q.java
@@ -4,9 +4,39 @@ package ProblemDay2;
 import java.io.*;
 import java.util.*;
 
-class Solution {
-    public int maxLen(int arr[]) {
-        
+class Solutionj {
+    public int maxLen(int arr[])
+    {
+        Map<Integer ,Integer > map = new HashMap<>();
+        int sum = 0 ;
+        int maxlen = 0 ;
+        for(int i = 0 ; i< arr.length ;i++){
+            System.out.println( "Arr"+arr[i]  +" "+i);
+            sum+=arr[i];
+            System.out.println("Sum "+sum+" "+i);
+
+
+            if(sum == 0){
+                System.out.println("Sum "+sum+" "+i);
+
+                maxlen = i+1;
+                System.out.println("MaxLen "+maxlen+" "+ i);
+            }
+            else if(map.containsKey(sum)){
+                int k = map.get(sum);
+                System.out.println("K "+sum+" "+i);
+                maxlen = Math.max(maxlen , i - k);
+                System.out.println("Last max len "+maxlen+" "+i);
+
+
+            }
+            else{
+                map.put(sum , i);
+                System.out.println( "Last else "+sum +" "+i );
+            }
+        }
+
+        return maxlen;
     }
 }
 
@@ -19,7 +49,7 @@ class Largest_Subarray_with_0_Sum {
         for (int i = 0; i < n; i++) {
             nums[i] = sc.nextInt();
         }
-        Solution Obj = new Solution();
+        Solutionj Obj = new Solutionj();
         System.out.println(Obj.maxLen(nums));
     }
 }
